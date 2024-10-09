93305cbeb6d8bff29d1e608224858c4ebd6596c1
goldpulpy <sawsani1928@gmail.com>
2024-10-09 04:04:46 +0300

    Added list of supported models

---
+++ pysentence_similarity/_support_models.py
@@ -0,0 +1,14 @@
+"""Support models list."""
+
+_support_models = [
+    "all-MiniLM-L6-v2",
+    "paraphrase-MiniLM-L6-v2",
+    "all-MiniLM-L12-v2",
+    "gte-small",
+    "all-mpnet-base-v2",
+    "paraphrase-multilingual-MiniLM-L12-v2"
+    "text2vec-base-multilingual",
+    "gte-multilingual-base",
+    "gte-large",
+    "LaBSE"
+]
